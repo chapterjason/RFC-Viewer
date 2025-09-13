d1c02c416cc4a0030f37328a36c8947ba22622b8
chapterjason <jason.schilling@hotmail.de>
2025-09-13 18:00:41 +0200

    Add manual test for renderer

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
 import * as fs from "node:fs";
 import {parse} from "./Tree/Parser.js";
 import {ArrayCursor} from "./Utils/ArrayCursor.js";
+import * as path from "node:path";
+import {renderDocument} from "./Tree/Render/RenderDocument.js";
 
+const currentWorkingDirectory = process.cwd();
+
+const rfcDirectory = path.join(currentWorkingDirectory, 'rfc');
+const outputDirectory = path.join(currentWorkingDirectory, 'output');
+
+fs.mkdirSync(rfcDirectory, {recursive: true});
+fs.mkdirSync(outputDirectory, {recursive: true});
 
 async function main() {
-    const content = fs.readFileSync('./rfc/rfc6749.txt', 'utf-8');
+    const rfcFile = path.join(rfcDirectory, 'rfc6749.txt');
+    const outputFile = path.join(outputDirectory, 'rfc6749.txt');
+
+    const content = fs.readFileSync(rfcFile, 'utf-8');
     const lines = content.replace(/\r?\n/g, '\n').split('\n');
     const cursor = new ArrayCursor(lines);
 
     const ast = parse(cursor);
+
+    const output = renderDocument(ast);
+
+    fs.writeFileSync(outputFile, output.join('\n'));
 
     console.dir(
         ast.children.filter(item => item.type === 'List')
