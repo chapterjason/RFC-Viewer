f5c357727287b9e5d92e4a216eca4d531db9a716
chapterjason <jason.schilling@hotmail.de>
2025-09-14 02:57:00 +0200

    Add support for processing multiple RFC files in a loop

--- src/index.ts
+++ src/index.ts
@@ -12,27 +12,42 @@ const outputDirectory = path.join(currentWorkingDirectory, 'output');
 fs.mkdirSync(rfcDirectory, {recursive: true});
 fs.mkdirSync(outputDirectory, {recursive: true});
 
+const numbers = [
+    6749,
+    6819,
+    7591,
+    7636,
+    8252,
+    8693,
+    9126,
+    9396,
+    6750,
+    7009,
+    7592,
+    7662,
+    8414,
+    9101,
+    9207,
+    9700,
+];
+
 async function main() {
-    const rfcFile = path.join(rfcDirectory, 'rfc6749.txt');
-    const originalOutputFile = path.join(outputDirectory, 'rfc6749.original.txt');
-    const outputFile = path.join(outputDirectory, 'rfc6749.txt');
-    const astFile = path.join(outputDirectory, 'rfc6749.json');
+    for (const number of numbers) {
+        const rfcFile = path.join(rfcDirectory, `rfc${number}.txt`);
+        const originalOutputFile = path.join(outputDirectory, `rfc${number}.original.txt`);
+        const outputFile = path.join(outputDirectory, `rfc${number}.txt`);
+        const astFile = path.join(outputDirectory, `rfc${number}.json`);
 
-    const content = fs.readFileSync(rfcFile, 'utf-8');
-    const lines = content.replace(/\r?\n/g, '\n').split('\n');
-    const cursor = new ArrayCursor(lines);
-    const ast = parse(cursor);
-    const output = renderDocument(ast);
+        const content = fs.readFileSync(rfcFile, 'utf-8');
+        const lines = content.replace(/\r?\n/g, '\n').split('\n');
+        const cursor = new ArrayCursor(lines);
+        const ast = parse(cursor);
+        const output = renderDocument(ast);
 
-    fs.writeFileSync(originalOutputFile, content);
-    fs.writeFileSync(astFile, JSON.stringify(ast, null, 2));
-    fs.writeFileSync(outputFile, output.join('\n'));
-
-    console.dir(ast, {
-        depth: null,
-        maxArrayLength: 5000,
-        compact: true,
-    });
+        fs.writeFileSync(originalOutputFile, content);
+        fs.writeFileSync(astFile, JSON.stringify(ast, null, 2));
+        fs.writeFileSync(outputFile, output.join('\n'));
+    }
 }
 
 
